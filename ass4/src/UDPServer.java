9d7f2b62da01802f069727a0d9708c0c341fee22
HHH6789 <3535777041@qq.com>
2025-06-14 18:24:08 +0800

    Max and min port number
    Maximum and minimum port number for data transfer

--- ass4/src/UDPServer.java
+++ ass4/src/UDPServer.java
@@ -8,5 +8,11 @@ import java.util.Random;
 // Main server class for UDP file transfers
 // UDP文件传输的主服务器类
 public class UDPServer {
-    
+    // Minimum port number for data transfer
+    // 数据传输的最小端口号
+    private static final int MIN_DATA_PORT = 50000;
+    // Maximum port number for data transfer
+    // 数据传输的最大端口号
+    private static final int MAX_DATA_PORT = 51000;
+
 }
