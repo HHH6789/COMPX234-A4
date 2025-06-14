7625b392ef8b60c1ac63d26a0a2477e8e6152d3c
HHH6789 <3535777041@qq.com>
2025-06-14 20:59:58 +0800

    UDPClient class

--- ass4/src/UDPClient.java
+++ ass4/src/UDPClient.java
@@ -2,3 +2,13 @@ import java.io.*;
 import java.net.*;
 import java.util.Base64;
 import java.util.concurrent.atomic.AtomicInteger;
+
+//UDPClient class
+//UDP客户端类
+public class UDPClient{
+    
+}
+
+
+
+
