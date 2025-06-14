300c072ce4d1bfae6e776f2d0eee6afaef3a5d95
HHH6789 <3535777041@qq.com>
2025-06-14 21:00:41 +0800

    max retries
    Maximum number of retries for sending/receiving packets

--- ass4/src/UDPClient.java
+++ ass4/src/UDPClient.java
@@ -6,7 +6,12 @@ import java.util.concurrent.atomic.AtomicInteger;
 //UDPClient class
 //UDP客户端类
 public class UDPClient{
-    
+    // Maximum number of retries for sending/receiving packets
+    // 发送/接收数据包的最大重试次数
+    private static final int MAX_RETRIES = 5;
+
+
+
 }
 
 
